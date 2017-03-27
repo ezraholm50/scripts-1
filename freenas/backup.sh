95bb284f17d8c537b4495d3369ee44beea25070c
Daniel Hansson <daniel@techandme.se>
2017-03-28 00:57:20 +0200

    working state

--- freenas/backup.sh
+++ freenas/backup.sh
@@ -1,48 +1,18 @@
-set -x
-
 #!/bin/sh
 
 currentdate=$(date +%Y-%m-%d_%H:%M)
 backupdir=/mnt/RAID10/cfgbackup
 backupname=config_backup_$currentdate
+keepfilesfordays=30
 
-# check if backup exists
-if [ -f $backupname ]
-then
-    # Create file
-    cli -e "system config download path=$backupdir/$backupname"
-    # Generate MD5
-    md5 $backupdir/$backupname | awk '{print $4}' > $backupdir/md5hash_orig
+# Do the backup and log on error or success
+if cli -e "system config download path=$backupdir/$backupname"; then
+   echo "Backup success $currentdate" >> $backupdir/config/log
 else
-    # Create file
-    cli -e "system config download path=$backupdir/$backupname"
-    # Generate MD5
-    md5 $backupdir/$backupname | awk '{print $4}' > $backupdir/md5hash_new
+   echo "Backup FAILED $currentdate" >> $backupdir/config/log
+   exit 1
 fi
 
-
-if ! diff config_backup_$currentdate config_backup_$currentdate >/dev/null 2>&1; then
-echo "hej"
-mv $backupname $currentdate
-else
-echo "not moving"
-fi
-
-
-
-######### TESTING ###########
-
-# Compare MD5 hashes
-#if ! cmp md5hash_orig md5hash_new >/dev/null 2>&1; then
-#echo "hej"
-#mv $backupname $currentdate
-#else
-#echo "not moving"
-#fi
-
-#if cmp -s "md5hash_orig" "md5hash_new"
-#then
-#   echo "The files match"
-#else
-#   echo "The files are different"
-#fi
+# Remove files older than $keepfilesfordays days
+echo "Removing files older than $keepfilesfordays days"
+find $backupdir -maxdepth 1 -type f -mtime +"$keepfilesfordays" | xargs rm
